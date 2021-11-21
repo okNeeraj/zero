8ab20c96b7847d195212e7d7f9948ca662cd7123
Neeraj Singh <neerajonetwo@gmail.com>
2021-11-22 03:33:32 +0530

    Project initiliazed

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,41 @@
 const mix = require('laravel-mix');
 
 /*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+    |--------------------------------------------------------------------------
+    | Frontend React App Compiler Settings
+    |--------------------------------------------------------------------------
+    | This line of code compile all backend CSS/JS and other resources.
+    |
+    */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// mix.js(
+//     'resources/Frontend/react/app.js',
+//     'public/Frontend/react')
+//     .react()
+//     .sass(
+//         'resources/Frontend/react/assets/sass/app.scss',
+//         'public/Frontend/react/css');
+
+
+/*
+    |--------------------------------------------------------------------------
+    | Backend Compiler Settings
+    |--------------------------------------------------------------------------
+    | This line of code compile all backend CSS/JS and other resources.
+    |
+    */
+
+mix.sass(
+    'resources/Backend/sass/app.scss',
+    'public/Backend/css')
+    .options({
+        processCssUrls: false
+    })
+    .sourceMaps(true, 'source-map');
+
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+mix.browserSync('http://127.0.0.1:8000/');
